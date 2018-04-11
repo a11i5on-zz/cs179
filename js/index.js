aff29ca168ee26ddb58a6d693e3bb13679265cd9
Allison <allison_lee@college.harvard.edu>
2018-04-11 14:21:55 -0400

    i don't even

--- js/index.js
+++ js/index.js
@@ -1,1 +1,2 @@
 document.getElementById("nameWelcome").innerHTML = "Welcome, " + localStorage["name"] + "!";
+document.getElementById("myBar").style.width = "parseInt(100*parseInt(localStorage['progress']) / parseInt(localStorage['goal']))" + "%"; 
