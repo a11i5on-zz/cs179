5f7adce7efdb7b5a400ba810048b7014d3ac7b75
Allison <allison_lee@college.harvard.edu>
2018-05-01 12:47:03 -0400

    small change

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ document.getElementById("nameWelcome").innerHTML = "Welcome, " + localStorage["n
 var percent = parseInt(100*parseInt(localStorage['progress']) / parseInt(localStorage['goal']));
 document.getElementById("myBar").style.width = percent + "%"; 
 document.getElementById("myBar").innerHTML = percent + "%"; 
-document.getElementById("statement").innerHTML = "You have earned $" + localStorage['progress'] + " this month so far. Great job!";
+document.getElementById("statement").innerHTML = "You have earned $" + localStorage['progress'] + " this month so far.";
 
 var x = document.getElementById("badgeID");
 if (localStorage["proofreadingBadge"] == 'true') {
